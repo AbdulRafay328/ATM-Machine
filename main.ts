95cb892a408d9996135836ac04b38c3cde774de0
AbdulRafay328 <rafaysheikhsf@gmail.com>
2024-05-13 15:59:18 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let mybalance = 10000;
+let mypin = 9876;
+
+let pinAnswer = await inquirer.prompt([
+  {
+    name: "pin",
+    type: "number",
+    message: "Enter your pin",
+  },
+]);
+
+if (pinAnswer.pin === mypin) {
+  console.log("correct pincode !!!");
+
+  let operationAns = await inquirer.prompt([
+    {
+      name: "operation",
+      type: "list",
+      message: "please select option",
+      choices: ["withdraw", "check balance"],
+    },
+  ]);
+
+  console.log(operationAns);
+  if (operationAns.operation === "withdraw") {
+    let amountAns = await inquirer.prompt([
+    { name: "amount", 
+      message: "Enter your amount", 
+      type: "number" 
+    },
+    ]);
+
+    mybalance -= amountAns.amount;
+
+    console.log(`your remaining balance is ${mybalance}`);
+    
+  }else if(operationAns.operation === "check balance"){
+    console.log(`your balance is ${mybalance}`);
+    
+  }
+} else {
+  console.log("Incorrect pincode!!!");
+}
